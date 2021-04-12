b52b9dd18ecacae47ea75b25454d2f4cc715aafa
Jess Dever <jessicadever1@gmail.com>
2021-04-12 14:01:58 -0500

    update uprepo

--- Gifter/Repositories/UserProfileRespository.cs
+++ Gifter/Repositories/UserProfileRespository.cs
@@ -47,7 +47,46 @@ namespace Gifter.Repositories
 
 
 
+        public UserProfile GetById(int id)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                          SELECT 
+                            up.Name, up.Bio, up.Email, up.DateCreated AS UserProfileDateCreated,
+                            up.ImageUrl AS UserProfileImageUrl, up.id AS PostUserProfileId
 
+                            FROM UserProfile up
+                            WHERE up.Id = @Id";
+
+                    DbUtils.AddParameter(cmd, "@Id", id);
+
+                    var reader = cmd.ExecuteReader();
+
+                    UserProfile profile = null;
+                    if (reader.Read())
+                    {
+                        profile = new UserProfile()
+                        {
+                            
+                                Id = DbUtils.GetInt(reader, "PostUserProfileId"),
+                                Name = DbUtils.GetString(reader, "Name"),
+                                Email = DbUtils.GetString(reader, "Email"),
+                                DateCreated = DbUtils.GetDateTime(reader, "UserProfileDateCreated"),
+                                ImageUrl = DbUtils.GetString(reader, "UserProfileImageUrl"),
+                            
+                        };
+                    }
+
+                    reader.Close();
+
+                    return profile;
+                }
+            }
+        }
 
 
 
