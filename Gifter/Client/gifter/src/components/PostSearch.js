87c72af5a722dc7fc1d1bd50e5df5e9de4b1168e
Jess Dever <jessicadever1@gmail.com>
2021-04-14 15:26:15 -0500

    update search

--- Gifter/Client/gifter/src/components/PostSearch.js
+++ Gifter/Client/gifter/src/components/PostSearch.js
@@ -1,26 +1,33 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useState } from "react";
 import { PostContext } from "../providers/PostProvider";
 
 const PostList = () => {
     const { searchByTitle } = useContext(PostContext);
     const [searchedTitle, setSearchedTitle] = useState("")
 
-    useEffect(() => {
-        search();
-    }, []);
 
-    const handleSearchBtnClick = () => {
-
+    const handleInputChange = (event) => {
+        setSearchedTitle(event.target.value)
     }
 
     return (
         <div className="container">
             <div className="row justify-content-center">
                 <label for="search">Search By Title:</label>
-                <input className="search" id="search" name="search" type="text" placeholder="Enter Title"></input>
-                <button onClick={handleSearchBtnClick}>Search</button>
+                <input
+                    className="search"
+                    id="search"
+                    name="search"
+                    type="text"
+                    placeholder="Enter Title"
+                    onclick={handleInputChange}
+                    value={searchedTitle}></input>
+                <button onClick={(event) => {
+                    event.preventDefault();
+                    searchByTitle(searchedTitle)
+                }}>Search</button>
             </div>
-        </div>
+        </div >
     );
 };
 
