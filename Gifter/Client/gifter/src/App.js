f57e750d9daa66b6a8ea3ebbb7a6be02363946f2
Jess Dever <jessicadever1@gmail.com>
2021-04-13 21:51:59 -0500

    chpt 6

--- Gifter/Client/gifter/src/App.js
+++ Gifter/Client/gifter/src/App.js
@@ -3,14 +3,21 @@ import "./App.css";
 import { PostProvider } from "./components/PostProvider";
 import PostList from "./components/PostList";
 import { PostForm } from "./components/PostForm";
+import { BrowserRouder as Router } from "react-router-dom";
+import ApplicationViews from "./components/ApplicationViews";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="App">
-      <PostProvider>
-        <PostForm />
-        <PostList />
-      </PostProvider>
+      <Router>
+        <PostProvider>
+          <Header />
+          <ApplicationViews />
+          <PostForm />
+          <PostList />
+        </PostProvider>
+      </Router>
     </div>
   );
 }
--- Gifter/Client/gifter/src/components/PostForm.js
+++ Gifter/Client/gifter/src/components/PostForm.js
@@ -1,70 +1,80 @@
-import React, { useContext, useEffect, useState } from "react"
-import { PostContext } from "./PostProvider"
+import React, { useState, useContext } from "react";
+import {
+    Form,
+    FormGroup,
+    Card,
+    CardBody,
+    Label,
+    Input,
+    Button,
+} from "reactstrap";
+import { PostContext } from "../providers/PostProvider";
+import { useHistory } from "react-router-dom";
 
-export const PostForm = () => {
+const PostForm = () => {
+    const { addPost } = useContext(PostContext);
+    const [userProfileId, setUserProfileId] = useState("");
+    const [imageUrl, setImageUrl] = useState("");
+    const [title, setTitle] = useState("");
+    const [caption, setCaption] = useState("");
 
-    const { addPost, getAllPosts } = useContext(PostContext)
+    // Use this hook to allow us to programatically redirect users
+    const history = useHistory();
 
-    const [post, setPost] = useState({
-        "id": 0,
-        "Title": "",
-        "ImageUrl": "",
-        "Caption": "",
-        "UserProfileId": 0,
-        "DateCreated": ""
-    })
+    const submit = (e) => {
+        const post = {
+            imageUrl,
+            title,
+            caption,
+            userProfileId: +userProfileId,
+        };
 
-    useEffect(() => {
-        getAllPosts(post.id)
-            .then(post => {
-                setPost(post)
-            })
-    }, [])
-
-    const handleClickSavePost = (event) => {
-        event.preventDefault()
-
-        addPost({
-            Title: post.Title,
-            ImageUrl: post.ImageUrl,
-            Caption: post.Caption,
-            UserProfileId: post.UserProfileId,
-            DateCreated: post.DateCreated
-        })
-    }
+        addPost(post).then((p) => {
+            // Navigate the user back to the home route
+            history.push("/");
+        });
+    };
 
     return (
-        <section className="">
-            <form className="">
-                <h2>New Post: </h2>
-                <fieldset>
-                    <div className="">
-                        <label htmlFor="title">Post title:</label>
-                        <input type="text" id="title" required autoFocus className="" placeholder="Post title" value={post.Title} />
-                    </div>
-                </fieldset>
-                <fieldset>
-                    <div className="">
-                        <label htmlFor="caption">Caption:</label>
-                        <input type="text" id="caption" required autoFocus className="" placeholder="Post caption" value={post.Caption} />
-                    </div>
-                </fieldset>
-                <fieldset>
-                    <div className="">
-                        <label htmlFor="date">Date Created:</label>
-                        <input type="date" id="date" required autoFocus className="" placeholder="" value={post.DateCreated} />
-                    </div>
-                </fieldset>
-                <div className="centerReviewSubmitBtn">
-                    <button id={post.id} className="btn btn-primary"
-                        onClick={
-                            handleClickSavePost
-                        }>
-                        Save Post
-                    </button>
-                </div>
-            </form>
-        </section>
-    )
+        <div className="container pt-4">
+            <div className="row justify-content-center">
+                <Card className="col-sm-12 col-lg-6">
+                    <CardBody>
+                        <Form>
+                            <FormGroup>
+                                <Label for="userId">User Id (For Now...)</Label>
+                                <Input
+                                    id="userId"
+                                    onChange={(e) => setUserProfileId(e.target.value)}
+                                />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for="imageUrl">Gif URL</Label>
+                                <Input
+                                    id="imageUrl"
+                                    onChange={(e) => setImageUrl(e.target.value)}
+                                />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for="title">Title</Label>
+                                <Input id="title" onChange={(e) => setTitle(e.target.value)} />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for="caption">Caption</Label>
+                                <Input
+                                    id="caption"
+                                    onChange={(e) => setCaption(e.target.value)}
+                                />
+                            </FormGroup>
+                        </Form>
+                        <Button color="info" onClick={submit}>
+                            SUBMIT
+            </Button>
+                    </CardBody>
+                </Card>
+            </div>
+        </div>
+    );
+};
 
-}
+export default PostForm;
